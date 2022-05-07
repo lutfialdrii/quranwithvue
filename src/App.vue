3eb4e1d63dd02ec4c5ba7cb3dcb6b1341a29e353
Lutfi Aldri Permana <12050110415@students.uin-suska.ac.id>
2022-05-07 08:01:44 +0700

    membuat  view dan mengaktifkan router

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,47 @@
+<template>
+  <!-- NavBar -->
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Mushaf Kita</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Baca</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"> Pilih Surah </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li v-for="chapter in listSurah" :key="chapter.id"><a class="dropdown-item" href="#">{{chapter.name_simple}}</a></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          </li>
+          <input v-model="nomor" type="search" placeholder="1-114" class="" aria-label="Search" />
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div v-for="chapter in listSurah" :key="chapter.id" class="row">
+    <div class="col-sm-6">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">{{chapter.name_simple}}</h5>
+          <p class="card-text">{{chapter.translated_name.name}}</p>
+          <a href="#" class="btn btn-primary">Baca</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+</template>
+
 <script>
 import { ref, watch } from "vue";
 import axios from "axios";
@@ -8,15 +52,15 @@ export default {
       nomor: "1",
       judul: "",
       arti: "",
-      listSurah:ref([]),
-      namaSurah:""
+      listSurah: ref([]),
+      namaSurah: "",
     };
   },
 
   watch: {
     nomor() {
       this.getSurah();
-    }
+    },
   },
 
   mounted() {
@@ -28,7 +72,7 @@ export default {
     getSurah() {
       axios
         .get(`https://api.quran.com/api/v4/chapters/${this.nomor}?language=id`)
-        .then(response => {
+        .then((response) => {
           this.judul = response.data.chapter;
           this.arti = this.judul.translated_name;
         })
@@ -36,62 +80,16 @@ export default {
           console.log(error);
         });
     },
-    getlistSurah(){
+    getlistSurah() {
       axios
-      .get(`https://api.quran.com/api/v4/chapters?language=en`)
-      .then(response => {
-        this.listSurah = response.data.chapters;
-      })
-      .catch(error => {
-        console.log(error)
-      })
-    }
-  }
+        .get(`https://api.quran.com/api/v4/chapters?language=id`)
+        .then((response) => {
+          this.listSurah = response.data.chapters;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
 };
 </script>
-
-<template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-        </li>
-        <input v-model="nomor" type="search" placeholder="1-114" class="" aria-label="Search" />
-      </ul>
-    </div>
-  </div>
-</nav>
-  <div>
-    <label for="surah">
-      <select class="form-control" v-model="selectedChapter">
-        <option v-for="chapter in listSurah" :key="chapter.id">{{chapter.name_simple}}</option>
-      </select>
-    </label>
-    <h1>Nama Surah :{{judul.name_simple}}</h1>
-    <p>Arti Surah :{{arti.name}}</p>
-    <input v-model="nomor" type="search" placeholder="1-114" class="form-control" aria-label="Search" />
-  </div>
-</template>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,97 @@
+<script>
+import { ref, watch } from "vue";
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      nomor: "1",
+      judul: "",
+      arti: "",
+      listSurah:ref([]),
+      namaSurah:""
+    };
+  },
+
+  watch: {
+    nomor() {
+      this.getSurah();
+    }
+  },
+
+  mounted() {
+    this.getSurah();
+    this.getlistSurah();
+  },
+
+  methods: {
+    getSurah() {
+      axios
+        .get(`https://api.quran.com/api/v4/chapters/${this.nomor}?language=id`)
+        .then(response => {
+          this.judul = response.data.chapter;
+          this.arti = this.judul.translated_name;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    getlistSurah(){
+      axios
+      .get(`https://api.quran.com/api/v4/chapters?language=en`)
+      .then(response => {
+        this.listSurah = response.data.chapters;
+      })
+      .catch(error => {
+        console.log(error)
+      })
+    }
+  }
+};
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </li>
+        <input v-model="nomor" type="search" placeholder="1-114" class="" aria-label="Search" />
+      </ul>
+    </div>
+  </div>
+</nav>
+  <div>
+    <label for="surah">
+      <select class="form-control" v-model="selectedChapter">
+        <option v-for="chapter in listSurah" :key="chapter.id">{{chapter.name_simple}}</option>
+      </select>
+    </label>
+    <h1>Nama Surah :{{judul.name_simple}}</h1>
+    <p>Arti Surah :{{arti.name}}</p>
+    <input v-model="nomor" type="search" placeholder="1-114" class="form-control" aria-label="Search" />
   </div>
 </template>
